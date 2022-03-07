061393d74806ca269b3f174cacdd1f6b16df3282
Devon Divinecz <w0723299@myscc.ca>
2022-03-07 14:29:34 -0500

    Created Show Models

---
+++ Models/Show.swift
@@ -0,0 +1,18 @@
+//
+//  Show.swift
+//  Midterm
+//
+//  Created by Devon Divinecz on 2022-03-07.
+//
+
+import Foundation
+
+struct Show: Codable, Hashable {
+    var id: Int
+    var collectionName: String
+    var image: String?
+    var trackName: String
+    var artistName: String
+    var price: Double
+    var description: String
+}
---
+++ Models/ShowStore.swift
@@ -0,0 +1,20 @@
+//
+//  ShowStore.swift
+//  Midterm
+//
+//  Created by Devon Divinecz on 2022-03-07.
+//
+
+import Foundation
+
+import Foundation
+
+class ShowStore {
+    var watchlist = [Show]()
+}
+
+import Foundation
+
+struct Shows: Codable {
+    var results: [Show]
+}
